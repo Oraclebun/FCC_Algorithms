a13423b18ff4624c7096ea7ffefdc4808c9ccf04
Mien Ling <mien_ling@hotmail.com>
2020-12-01 07:17:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function sym(args) {
+  let argv = [...arguments];
+  let setOfTwo=argv[0];
+  function sym2(a,b){
+  let result = b.filter(x => {
+    return !a.includes(x);
+  }).concat(a.filter(el =>{
+    return !b.includes(el);
+  }));
+  return [...new Set(result)];
+}
+
+for (let i=0; i<argv.length-1; i++){
+    setOfTwo = sym2(setOfTwo, argv[i+1])
+  }
+  return setOfTwo;
+
+}
+
+sym([1, 2, 5], [2, 3, 5], [ 3, 4, 5]);
